6351e07beb88153058c5e64443db8b4332a0ca44
Cr2do <credoay@gmail.com>
2022-06-11 19:02:58 +0100

    correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ if number % 10 > 5:
     greaterVal = "is {} and is greater than 5"
     print(valueGlobal.format(number), greaterVal.format(number % 10))
 elif number % 10 == 0:
-    zeroVal = "is {} and is greater than 5"
+    zeroVal = "is {} and is 0"
     print(valueGlobal.format(number), zeroVal.format(number % 10))
 else:
-    lowerVal = "is {} and is greater than 5"
+    lowerVal = "is {} and is less than 6 and not 0"
     print(valueGlobal.format(number), lowerVal.format(number % 10))
