e0972fc4199a1b9ce3182337b24b5215885e5b4c
Cr2do <credoay@gmail.com>
2022-06-12 11:20:36 +0100

    Task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, div, sub, mul
+    import sys
+    if len(sys.argv) - 1 == 3:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            print(sys.exit(0))
+        elif sys.argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print(sys.exit(0))
+        elif sys.argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print(sys.exit(0))
+        elif sys.argv[2] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+            print(sys.exit(0))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+    else:        
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print(sys.exit(1))
