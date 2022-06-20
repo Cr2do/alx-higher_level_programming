be1a3cbbd79e784081c322b9e3d436df40941be6
Cr2do <credoay@gmail.com>
2022-06-20 23:58:15 +0100

    Safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     return_val = 0
-    for (k, v) in enumerate(my_list):        
-        print("{}".format(v), end="")
-        return_val = return_val + 1
+    for (k, v) in enumerate(my_list):
+        if k < x:
+            print("{}".format(v), end="")
+            return_val = return_val + 1
     print("")
     return x if return_val == x else return_val
