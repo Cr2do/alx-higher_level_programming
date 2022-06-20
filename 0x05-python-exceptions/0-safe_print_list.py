a3d8af7ace1593646d5c86cc5295fbc4dd4aa18c
Cr2do <credoay@gmail.com>
2022-06-21 00:11:03 +0100

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
                 return_val = return_val + 1
         print("")
         return x if return_val == x else return_val
-    except:
-        print("Exception found")
+    except Exception as e:
+        print("{}".format(e))
