4835f76bcb0cc040934a9f65d4d921919231cedd
Cr2do <credoay@gmail.com>
2022-06-21 23:20:37 +0100

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    return_val = 0
+    for v in range(x):
+        try:        
+            print("{:d}".format(my_list[v]), end="")
+            return_val += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return return_val
