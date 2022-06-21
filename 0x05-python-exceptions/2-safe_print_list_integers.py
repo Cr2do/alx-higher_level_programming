8ee649b959ff6b0ce764e3f805b7ce1ade5bf583
Cr2do <credoay@gmail.com>
2022-06-21 23:22:39 +0100

    Pycode style on task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     return_val = 0
     for v in range(x):
-        try:        
+        try:
             print("{:d}".format(my_list[v]), end="")
             return_val += 1
         except (ValueError, TypeError):
