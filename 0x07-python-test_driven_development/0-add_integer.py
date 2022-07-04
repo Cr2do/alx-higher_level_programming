0d7e665821bd491d849213765624b403d7bbea7d
Cr2do <credoay@gmail.com>
2022-07-04 01:44:31 +0100

    task test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,8 +5,12 @@ add_integer module
 
 
 def add_integer(a, b=98):
-    """
-    add_integer
+    """ add_integer
+    Args:
+        a: first parameter
+        b: second parameter
+    Returns:
+        result a sum
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
