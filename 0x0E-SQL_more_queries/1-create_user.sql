cae30ef3f44de499813ca18696b6a1dc4943d0fc
Cr2do <credoay@gmail.com>
2022-08-10 23:52:02 +0100

    TASK  AND 2

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL on *.* TO `user_0d_1`@`localhost`;
