de0ca080ff4901f30c2790c91b3920ecb01f7a38
Cr2do <credoay@gmail.com>
2022-08-10 23:58:52 +0100

    READ User role

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,3 @@
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT on `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
