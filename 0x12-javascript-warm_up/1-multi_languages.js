f864fe84ad02f88dd04717b637e4362e5431f5b3
Cr2do <credoay@gmail.com>
2022-09-06 02:16:21 +0100

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-let myVar = 'C is fun\nPython is cool\nJavascript is amazing'
-console.log(myVar)
+/* script tha prints 3 lines: */
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myVar);
